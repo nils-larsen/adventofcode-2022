5f9b0dffb74890e2b71614b23e1bb02e2bb35f40
Nils Larsén <nilsfredriklarsen@gmail.com>
2022-12-01 11:03:02 +0100

    day 1 completed

---
+++ tests/days/day01/Day1.cs
@@ -0,0 +1,64 @@
+namespace days.day01;
+
+public class Day1
+{
+    private const string InputFile = "day01/day1.txt";
+
+    [Fact]
+    public void The_Elf_Carrying_The_Most_Calories_Carries_24000()
+    {
+        const string testData = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
+
+        testData
+            .Split($"{Environment.NewLine}{Environment.NewLine}")
+            .Select(x => x.Split(Environment.NewLine).Select(int.Parse))
+            .Select(x => x.Aggregate((sum, val) => sum + val))
+            .Max()
+            .Should()
+            .Be(24000);
+    }
+    
+    [Fact]
+    public void Part1_The_Elf_Carrying_The_Most_Calories_Carries_66719()
+    {
+        File
+            .ReadAllText(InputFile)
+            .Split($"{Environment.NewLine}{Environment.NewLine}")
+            .Select(x => x.Split(Environment.NewLine).Select(int.Parse))
+            .Select(x => x.Aggregate((sum, val) => sum + val))
+            .Max()
+            .Should()
+            .Be(66719);
+    }
+    
+    [Fact]
+    public void The_Top_3_Most_Carrying_Elves_Carries_Should_Carry_45000()
+    {
+        const string testData = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
+
+        testData
+            .Split($"{Environment.NewLine}{Environment.NewLine}")
+            .Select(x => x.Split(Environment.NewLine).Select(int.Parse))
+            .Select(x => x.Aggregate((sum, val) => sum + val))
+            .OrderDescending()
+            .Take(3)
+            .Aggregate((sum, val) => sum + val)
+            .Should()
+            .Be(45000);
+    }
+    
+    [Fact]
+    public void Part2_The_Top_3_Most_Carrying_Elves_Carries_Should_Carry_198551()
+    {
+        File
+            .ReadAllText(InputFile)
+            .Split($"{Environment.NewLine}{Environment.NewLine}")
+            .Select(x => x.Split(Environment.NewLine).Select(int.Parse))
+            .Select(x => x.Aggregate((sum, val) => sum + val))
+            .OrderDescending()
+            .Take(3)
+            .Aggregate((sum, val) => sum + val)
+            .Should()
+            .Be(198551);
+    }
+}
